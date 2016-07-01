b8cddbb99f201a519275ba9778e9000ee91ae4b5
goWithHappy <2440138281@qq.com>
2016-07-01 14:17:01 +0800

    Test eclipse

--- SORM/src/beans/TableInfo.java
+++ SORM/src/beans/TableInfo.java
@@ -15,11 +15,15 @@ public class TableInfo {
 	/**
 	 * 所有字段的信息
 	 */
-	private Map<String,ColumnInfo> columns; 
+	private Map<String,ColumnInfo> column; 
 	/**
 	 * 唯一主键（目前只处理唯一主键的情况）
 	 */
 	private ColumnInfo onlyPriKey;
+	/**
+	 * 如果是联合主键便在这里进行存储
+	 */
+	private Map<String,ColumnInfo> columns;
 	public String getTname() {
 		return tname;
 	}
@@ -27,10 +31,10 @@ public class TableInfo {
 		this.tname = tname;
 	}
 	public Map<String, ColumnInfo> getColumns() {
-		return columns;
+		return column;
 	}
 	public void setColumns(Map<String, ColumnInfo> columns) {
-		this.columns = columns;
+		this.column = columns;
 	}
 	public ColumnInfo getOnlyPriKey() {
 		return onlyPriKey;
@@ -41,7 +45,7 @@ public class TableInfo {
 	public TableInfo(String tname, Map<String, ColumnInfo> columns, ColumnInfo onlyPriKey) {
 		super();
 		this.tname = tname;
-		this.columns = columns;
+		this.column = columns;
 		this.onlyPriKey = onlyPriKey;
 	}
 	public TableInfo() {
